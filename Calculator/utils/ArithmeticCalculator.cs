557e818cf5f50d18a96a471d8f29e7986911fe7c
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-09-10 00:12:34 +0300

    Added ArithmeticCalculator util

---
+++ Calculator/utils/ArithmeticCalculator.cs
@@ -0,0 +1,39 @@
+using System;
+
+public static class ArithmeticCalculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double ChangeSign(double a)
+    {
+        return -a;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+
+    public static double Inverse(double a)
+    {
+        return 1 / a;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double SquareRoot(double a)
+    {
+        return Math.Sqrt(a);
+    }
+}
