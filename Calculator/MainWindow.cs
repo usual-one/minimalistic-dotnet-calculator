738859b782a9570f46ccfeb5022676a2ef3db9fb
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-09-09 23:30:12 +0300

    initial commit

---
+++ Calculator/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        this.OneButton.KeyPressEvent += new global::Gtk.KeyPressEventHandler(this.AddOne);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void AddOne(object sender, KeyPressEventArgs a)
+    {
+        string value = this.MainOutput.Text;
+        this.MainOutput.Text = value + sender.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# minimalistic-dotnet-calculator
+# Minimalistic .NET Calculator
+
 Simple .NET Core calculator for BMSTU APL course
+
+# Used languages, libraries & frameworks
+
+This project uses:
+- C#
+- .NET Core
+- Gtk 2
